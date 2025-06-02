825922572c0c70eab79041c37dbd86054d1001cf
Denis Mikaya <denis.mikaya@unblu.com>
2025-06-02 18:52:44 +0200

    Package.swift

---
+++ Package.swift
@@ -0,0 +1,72 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let version = "4.10.1"
+
+let UnbluCoreSDK_CHECKSUM                     = "4369a7240eb28fe34b77fbc2bcf320aecbb581bfb2ec63266a9a9475429a7a2c"
+let UnbluFirebaseNotificationModule_CHECKSUM   = "f7dd5487bfe62ca51359295b62b8db872c2aadde052c0219e6ecb5a7726dd2ad"
+let UnbluOpenTokCallModule_CHECKSUM            = "5b0893fc4020bedb13b867a69d78fc2b1b91c67c8f16359bd0619067214f4d69"
+let UnbluLiveKitCallModule_CHECKSUM            = "d8ce922ccc6be694f68d74d779e88b008740c3bddf4acf007d970e4707c800df"
+let UnbluMobileCoBrowsing_CHECKSUM              = "667bcb19ddedca4f34e0c9399f9007b17799252abaa57b029ff147087a190b38"
+let UnbluCallKitModule_CHECKSUM                 = "e9978448930b1fe55d005db8b2e4dee4bcfc2f3fb69cb329df65aac995dc3cb6"
+
+let package = Package(
+    name: "UnbluMobileSDKWrapper",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "UnbluMobileSDK",
+            targets: ["UnbluCoreSDK", "UnbluMobileCoBrowsingModule"]
+        ),
+        .library(
+            name: "UnbluFirebaseNotificationModule",
+            targets: ["UnbluFirebaseNotificationModule"]
+        ),
+        .library(
+            name: "UnbluLiveKitWebRtcProvider",
+            targets: ["UnbluLiveKitCallModule"]
+        ),
+        .library(
+            name: "UnbluVonageWebRtcProvider",
+            targets: ["UnbluOpenTokCallModule"]
+        ),
+        .library(
+            name: "UnbluCallKitModule",
+            targets: ["UnbluCallKitModule"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "UnbluCoreSDK",
+            url: "https://github.com/unblu/ios-sdk-xcframeworks/releases/download/\(version)/UnbluCoreSDK.xcframework.zip",
+            checksum: UnbluCoreSDK_CHECKSUM
+        ),
+        .binaryTarget(
+            name: "UnbluFirebaseNotificationModule",
+            url: "https://github.com/unblu/ios-sdk-xcframeworks/releases/download/\(version)/UnbluFirebaseNotificationModule.xcframework.zip",
+            checksum: UnbluFirebaseNotificationModule_CHECKSUM
+        ),
+        .binaryTarget(
+            name: "UnbluOpenTokCallModule",
+            url: "https://github.com/unblu/ios-sdk-xcframeworks/releases/download/\(version)/UnbluOpenTokCallModule.xcframework.zip",
+            checksum: UnbluOpenTokCallModule_CHECKSUM
+        ),
+        .binaryTarget(
+            name: "UnbluLiveKitCallModule",
+            url: "https://github.com/unblu/ios-sdk-xcframeworks/releases/download/\(version)/UnbluLiveKitCallModule.xcframework.zip",
+            checksum: UnbluLiveKitCallModule_CHECKSUM
+        ),
+        .binaryTarget(
+            name: "UnbluMobileCoBrowsingModule",
+            url: "https://github.com/unblu/ios-sdk-xcframeworks/releases/download/\(version)/UnbluMobileCoBrowsingModule.xcframework.zip",
+            checksum: UnbluMobileCoBrowsing_CHECKSUM
+        ),
+        .binaryTarget(
+            name: "UnbluCallKitModule",
+            url: "https://github.com/unblu/ios-sdk-xcframeworks/releases/download/\(version)/UnbluCallKitModule.xcframework.zip",
+            checksum: UnbluCallKitModule_CHECKSUM
+        )
+    ]
+)
